32c0b216c874b6dbf26c6e556f051d23dbd49223
junnyxi <junnyxi@163.com>
2017-02-21 17:36:49 +0800

    Add files via upload

---
+++ lr-wsserver.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
+	"net/http"
+	"time"
+)
+
+const (
+	LISTENADDR = "localhost:1234"
+)
+
+var(
+	clientMap map[string]*websocket.Conn
+)
+
+func getHeartBeat(ws *websocket.Conn) {
+
+	clientStr := ws.Request().RemoteAddr
+	fmt.Println("[debug] client a:"+ clientStr)
+
+	if _, ok := clientMap[clientStr]; ok {
+		log.Println("client is in")
+	} else {
+		log.Println("client is no in")
+		clientMap[clientStr] = ws
+		sendBroadcast(nil, fmt.Sprintf("Hello, %s join\n", clientStr))
+		for ck, _ := range clientMap {
+			log.Println("c in map: ", ck)
+		}
+	}
+
+	defer func(){
+		if err := ws.Close(); err != nil {
+			log.Println("[err] Websocket could't bu closed,", err.Error())
+		} else {
+			if _, ok := clientMap[clientStr]; ok {
+				delete(clientMap, clientStr)
+			}
+			log.Println("websocket closed.", clientStr)
+		}
+	}()
+
+	for {
+		var reply string
+		if err := websocket.Message.Receive(ws, &reply); err != nil {
+			log.Println("[err] Can't recieve", err.Error())
+			return
+		}
+
+		fmt.Println("[debug] client b:"+ ws.Request().RemoteAddr)
+
+		clientMap[ws.Request().RemoteAddr] = ws
+
+		log.Println("[info] Recieved back from client["+ws.Request().RemoteAddr +"]: " + reply)
+		t := time.Now()
+		msg := fmt.Sprintf("Recieved: %s, // %s", reply, t.Format(time.RFC3339))
+
+		log.Println("[info] Sending to client:" + msg)
+
+		sendBroadcast(ws, msg)
+	}
+}
+
+func sendBroadcast(conn *websocket.Conn, msg string){
+	for ck, client := range clientMap {
+		if conn == client {
+			log.Println("Continue....")
+//			continue
+		}
+		log.Printf("send => %s ", ck)
+
+		websocket.Message.Send(client, msg)
+	}
+}
+
+func getClientMap(w http.ResponseWriter, r *http.Request){
+	for ck, _ := range clientMap {
+		log.Println("Client" ,ck)
+	}
+}
+
+func main() {
+	clientMap =  make(map[string]*websocket.Conn)
+	http.Handle("/", websocket.Handler(getHeartBeat))
+
+	http.HandleFunc("/cm", getClientMap)
+
+	if err := http.ListenAndServe(":1234", nil); err != nil {
+		log.Fatal("ListenAndServer:", err)
+	}
+}
